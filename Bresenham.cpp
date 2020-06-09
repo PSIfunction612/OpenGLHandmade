43c5decffee2d8b17da863e6eab82ada9488d3f8
Maxim <shalaginmx@mail.ru>
2020-06-09 09:14:58 +0300

    net render

---
+++ Bresenham.cpp
@@ -0,0 +1,37 @@
+//
+// Created by shala on 08.06.2020.
+//
+#include "external/tgaimage.h"
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color){
+    bool steep = false;
+    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1){
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int dx = x1-x0;
+    int dy = y1-y0;
+    float derror = std::abs(dy/float(dx));
+    float error = 0.f;
+    int y = y0;
+
+    for(int x=x0; x<=x1; x++){
+        if (steep){
+            image.set(y,x, color);
+        } else {
+            image.set(x,y, color);
+        }
+        error += derror;
+
+        if (error > .5f) {
+            y += (y1 > y0?1:-1);
+            error = -1.f;
+        }
+    }
+}
